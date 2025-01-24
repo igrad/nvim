4358e929847bfab044b0d8ce112d8ab934bf9409
Ian Gradert <iangradert@gmail.com>
2025-01-23 22:28:44 -0600

    Fixed conform for home use

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,13 +13,14 @@ return { -- Autoformat
 		},
 	},
 	opts = {
-		notify_on_error = false,
+		notify_on_error = true,
+		log_level = vim.log.levels.DEBUG,
 		format_on_save = function(bufnr)
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
 			-- local disable_filetypes = { c = true, cpp = true }
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = { c = false, cpp = false }
 			local lsp_format_opt
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				lsp_format_opt = "never"
@@ -32,16 +33,23 @@ return { -- Autoformat
 			}
 		end,
 		formatters_by_ft = {
-			cpp = {
-				"clang-format",
-				args = { "-style=/workspace/guidance-ci/guidance_repo/JohnDeereCPPCodeStyle.clang-format" },
-			},
+			cpp = { "clang_format" },
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		formatters = {
+			clang_format = {
+				command = "clang-format",
+				append_args = function()
+					return {
+						"--style=file:/home/ian/projects/artificer/artificer-cpp-style.clang-format",
+					}
+				end,
+			},
 		},
 	},
 }
